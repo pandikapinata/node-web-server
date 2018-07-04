2bb19ea1280a08338994444fbd9ca274fda962ba
pandikapinata <pandikapinata27@gmail.com>
2018-07-04 16:17:56 +0800

    Rendering Template with Data

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/',(req, res) => {
     // res.send('<h1> Hello Express</h1>');
-    res.send({
-        name: 'Pandika',
-        likes: [
-            'Gaming',
-            'Football'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeText: 'Welcome to Home Page'
     });
 });
 
 app.get('/about', (req, res) =>{
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) =>{
@@ -22,4 +27,6 @@ app.get('/bad', (req, res) =>{
         errorMessage:'Unable to fetch data'
     });
 });
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+});
